13bba7c16d63a96965699060dda856227e411b8e
Alex Sales <asales@ewise.com>
2015-08-31 00:48:26 +0800

    minimal final

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -23,13 +23,16 @@ object Hello {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
     val now = System.nanoTime
+    val urlSource: BufferedSource = Source.fromURL("http://s3-ap-southeast-1.amazonaws.com/geeks.redmart.com/coding-problems/map.txt")
+    val res: String = urlSource.mkString
+
 
     //SEMI-REAL TEST
-    val resBuilder: StringBuilder = new StringBuilder("4 4\n")
-    resBuilder.append("4 8 7 3\n")
-    resBuilder.append("2 5 9 3\n")
-    resBuilder.append("6 3 2 5\n")
-    resBuilder.append("4 4 1 6\n")
+//    val resBuilder: StringBuilder = new StringBuilder("4 4\n")
+//    resBuilder.append("4 8 7 3\n")
+//    resBuilder.append("2 5 9 3\n")
+//    resBuilder.append("6 3 2 5\n")
+//    resBuilder.append("4 4 1 6\n")
 
     //TEST PASSED
 //    val resBuilder: StringBuilder = new StringBuilder("4 4\n")
@@ -69,7 +72,7 @@ object Hello {
 //      val resBuilder: StringBuilder = new StringBuilder("4 4\n")
 //      resBuilder.append("3 5 2\n")
 
-    val res = resBuilder.toString()
+//    val res = resBuilder.toString()
 //    println(res)
 
     var perLine: Array[String] = res.split("\n")
